c8a1056bc20bd7637ded40163cba67bfbc8920db
DeanDal1000 <dalzelldean@gmail.com>
2021-12-30 12:33:04 +0000

    Added Live Tracker

--- src/components/Livetracker.js
+++ src/components/Livetracker.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Container, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    paddingTop: theme.spacing(10),
+  },
+}));
 
 const Livetracker = () => {
+  const classes = useStyles();
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=20&page=1&sparkline=false'
+      )
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((error) => console.log('error'));
+  }, []);
+
   return (
-    <div>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat aut saepe
-      laboriosam cupiditate velit numquam non officia eius assumenda alias,
-      facere dolore, et eum autem distinctio ratione, repellat necessitatibus?
-      Quod.lore Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni
-      voluptates, cum dolorem doloremque suscipit dolorum et reiciendis
-      provident, molestiae saepe earum cupiditate perspiciatis vero perferendis
-      nihil! Beatae repudiandae ipsam a!
-    </div>
+    <Container className={classes.container}>
+      <div className="coin-search">
+        <h1>Live Tracker</h1>
+        <form>
+          <input type="text" placeholder="Search" className="coin-input" />
+        </form>
+      </div>
+    </Container>
   );
 };
 
